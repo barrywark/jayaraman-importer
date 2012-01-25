696cca905aa48c9906199ef7e8a170b1d5e39352
Barry Wark <barrywark@gmail.com>
2012-01-25 10:27:15 -0500

    Basic runtestsuite updates
    Take path to test folder.

---
+++ test/TestAppendXSG.m
@@ -0,0 +1,13 @@
+classdef TestAppendXSG < TestBase
+    properties
+        
+    end
+    
+    methods
+        function self = TestAppendXSG(name)
+            self = self@TestBase(name);
+        end
+        
+
+    end
+end
--- test/runtestsuite.m
+++ test/runtestsuite.m
@@ -1,44 +1,46 @@
-% This script builds a new Ovation database for testing and imports the
-% test fixture. Run this script from the pladps-importer/test directory.
-%
-% After running runtestsuite, you may run runtests (the Matlab xUnit test
-% runner) to re-run the test suite without building a new database or
-% re-importing the test fixture data.
-
-
-% N.B. these values should match in TestPldapsBase
-connection_file = 'ovation/matlab_test.connection';
-username = 'TestUser';
-password = 'password';
-
-% We're tied to the test fixture defined by these files, but this is the
-% only dependency. There shouldn't be any magic numbers in the test code.
-xsgFile = 'fixtures/AA0001AAAA0003.xsg';
-
-% Delete the test database if it exists
-if(exist(connection_file, 'file') ~= 0)
-    ovation.util.deleteLocalOvationDatabase(connection_file);
+function runtestsuite(test_folder)
+    % This script builds a new Ovation database for testing and imports the
+    % test fixture. Run this script from the pladps-importer/test directory.
+    %
+    % After running runtestsuite, you may run runtests (the Matlab xUnit test
+    % runner) to re-run the test suite without building a new database or
+    % re-importing the test fixture data.
+    
+    
+    % N.B. these values should match in TestPldapsBase
+    connection_file = [test_folder '/ovation/matlab_test.connection'];
+    username = 'TestUser';
+    password = 'password';
+    
+    % We're tied to the test fixture defined by these files, but this is the
+    % only dependency. There shouldn't be any magic numbers in the test code.
+    xsgFile = [test_folder '/fixtures/AA0001AAAA0003.xsg'];
+    
+    % Delete the test database if it exists
+    if(exist(connection_file, 'file') ~= 0)
+        ovation.util.deleteLocalOvationDatabase(connection_file);
+    end
+    
+    % Create a test database
+    system('mkdir -p ovation');
+    
+    connection_file = ovation.util.createLocalOvationDatabase([test_folder '/ovation'], ...
+        'matlab_test',...
+        username,...
+        password,...
+        'license.txt',...
+        'ovation-development');
+    
+    import ovation.*
+    ctx = Ovation.connect(connection_file, username, password);
+    project = ctx.insertProject('TestImportMapping',...
+        'TestImportMapping',...
+        datetime());
+    
+    expt = project.insertExperiment('TestImportMapping',...
+        datetime());
+    source = ctx.insertSource('animal');
+    
+    
+    runtests(test_folder, '-xmlfile', 'test-output.xml');
 end
-
-% Create a test database
-system('mkdir -p ovation');
-
-connection_file = ovation.util.createLocalOvationDatabase('ovation', ...
-    'matlab_test',...
-    username,...
-    password,...
-    'license.txt',...
-    'ovation-development');
-
-import ovation.*
-ctx = Ovation.connect(connection_file, username, password);
-project = ctx.insertProject('TestImportMapping',...
-    'TestImportMapping',...
-    datetime());
-
-expt = project.insertExperiment('TestImportMapping',...
-    datetime());
-source = ctx.insertSource('animal');
-
-
-runtests -xmlfile test-output.xml
