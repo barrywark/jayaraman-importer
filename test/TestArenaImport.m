538cd8a7d47198c65b9a82aabd187cfa63cba82d
Barry Wark <barrywark@gmail.com>
2012-02-09 13:56:28 -0500

    Complete Fly Arena import and test suite.
    Squashed commit of the following:
    
    commit d21a3b5bc86a762ddbc310a904401492c6c7df62
    Author: Barry Wark <barrywark@gmail.com>
    Date:   Thu Feb 9 13:28:30 2012 -0500
    
        Complete direct response all tests pass.
    
    commit 99217795ae972f8ae85140f26523cbbbf3c57037
    Author: Barry Wark <barrywark@gmail.com>
    Date:   Thu Feb 9 13:22:12 2012 -0500
    
        Direct response
    
    commit b0c3a331d11313c27d184b49737b5c4072fba23e
    Author: Barry Wark <barrywark@gmail.com>
    Date:   Thu Feb 9 12:54:29 2012 -0500
    
        XSG channel references
    
    commit 56d9b15ded01cbab3dd0ada5bf7f054711196012
    Author: Barry Wark <barrywark@gmail.com>
    Date:   Thu Feb 9 12:20:08 2012 -0500
    
        Test stimulus resource addition

--- test/TestArenaImport.m
+++ test/TestArenaImport.m
@@ -1,5 +1,5 @@
 classdef TestArenaImport < TestBase
-
+    
     properties
         
     end
@@ -20,66 +20,70 @@ classdef TestArenaImport < TestBase
             experiment = experiments(1);
             sources = self.context.getSources();
             source = sources(1);
-                       
+            
             self.epochGroup = experiment.insertEpochGroup(source,...
                 'test epoch group',...
                 datetime());
         end
         
         function [trial] = trialStruct(self, protocolID)
-           
-           trial.epochStartTime = ovation.datetime();
-           trial.epochEndTime = trial.epochStartTime.plusMillis(10003);
-           
-           trial.protocolID = protocolID;
-           
-           params.param1 = 1;
-           params.param2 = 'abc';
-           trial.protocolParameters = params;
-           
-           trial.arena.controllerMode = 1;
-           trial.arena.firmwareVersion = 2;
-           trial.arena.controllerParameters.param1 = 'foo';
-           trial.arena.controllerParameters.param2 = 10;
-           trial.arena.arenaConfigurationName = 'test-configuration';
-           trial.arena.controllerParameters.foo = 'bar';
-           
-           trial.arena.frameNumber = [1,2,3];
-           
-           trial.arena.patternParameters.param1 = 'foo';
-           trial.arena.patternParameters.param2 = 10;
-           
-           trial.arena.patternFile = 'TestArenaImportPattern.mat';
-           pattern = struct();
-           save(trial.arena.patternFile, 'pattern');
-           
-           trial.arena.patternGenerationFunction = 'TestArenaImport.m';
-           
+            
+            % This suite is bound to the XSG fixture
+            trial.epochStartTime = ovation.datetime(2011,10,18,12,31,27, 0, 'America/New_York');
+            trial.epochEndTime = trial.epochStartTime.plusMillis(42030);
+            
+            trial.protocolID = protocolID;
+            
+            params.param1 = 1;
+            params.param2 = 'abc';
+            trial.protocolParameters = params;
+            
+            trial.arena.controllerMode = 1;
+            trial.arena.firmwareVersion = 2;
+            trial.arena.controllerParameters.param1 = 'foo';
+            trial.arena.controllerParameters.param2 = 10;
+            trial.arena.arenaConfigurationName = 'test-configuration';
+            trial.arena.controllerParameters.foo = 'bar';
+            
+            trial.arena.frameNumber = [1,2,3];
+            
+            trial.arena.patternParameters.param1 = 'foo';
+            trial.arena.patternParameters.param2 = 10;
+            
+            trial.arena.patternFile = 'TestArenaImportPattern.mat';
+            pattern = struct(); %#ok<NASGU>
+            save(trial.arena.patternFile, 'pattern');
+            
+            trial.arena.patternGenerationFunction = 'TestArenaImport.m';
+            trial.arena.arenaConfigurationFile = 'TestArenaImportArenaConfig.mat';
+            config = struct(); %#ok<NASGU>
+            save(trial.arena.arenaConfigurationFile, 'config');
+            
         end
         
         
         function testImportEpochProtocolParameters(self)
-           
-           protocolID = 'testImportEpochProtocolParameters';
-           
-           trial = self.trialStruct(protocolID);
-           params = trial.protocolParameters;
-           
-           [epoch,~] = insertFlyArenaEpoch(self.epochGroup,...
-               trial);
-           
-           assert(~isempty(epoch));
-           assert(epoch.getEpochGroup().getUuid().equals(self.epochGroup.getUuid()));
-           
-           assert(strcmp(char(epoch.getProtocolID()), protocolID));
-           
-           actual = ovation.map2struct(epoch.getProtocolParameters());
-           
-           assert(params.param1 == actual.param1);
-           assert(strcmp(params.param2, actual.param2));
-           
-           assert(trial.epochStartTime.equals(epoch.getStartTime()));
-           assert(trial.epochEndTime.equals(epoch.getEndTime()));
+            
+            protocolID = 'testImportEpochProtocolParameters';
+            
+            trial = self.trialStruct(protocolID);
+            params = trial.protocolParameters;
+            
+            [epoch,~] = insertFlyArenaEpoch(self.epochGroup,...
+                trial);
+            
+            assert(~isempty(epoch));
+            assert(epoch.getEpochGroup().getUuid().equals(self.epochGroup.getUuid()));
+            
+            assert(strcmp(char(epoch.getProtocolID()), protocolID));
+            
+            actual = ovation.map2struct(epoch.getProtocolParameters());
+            
+            assert(params.param1 == actual.param1);
+            assert(strcmp(params.param2, actual.param2));
+            
+            assert(trial.epochStartTime.equals(epoch.getStartTime()));
+            assert(trial.epochEndTime.equals(epoch.getEndTime()));
         end
         
         function testStimulusInsertion(self)
@@ -159,13 +163,191 @@ classdef TestArenaImport < TestBase
             [epoch,~] = insertFlyArenaEpoch(self.epochGroup,...
                 trial);
             
+            resourceNames = epoch.getStimulus('Fly Arena').getResourceNames();
+            
+            self.assertResource(resourceNames,...
+                trial.arena.patternFile);
+            
+            self.assertResource(resourceNames,...
+                trial.arena.patternGenerationFunction);
+            
+            self.assertResource(resourceNames,...
+                trial.arena.arenaConfigurationFile);
             
         end
         
+        function testShouldCreateXSGChannelLinks(self)
+            protocolID = 'testShouldCreateXSGChannelLinks';
+            
+            trial = self.trialStruct(protocolID);
+            
+            trial.arena.xsgXSequenceChannel = 'XSignalArena';
+            trial.arena.xsgYSequenceChannel = 'YSignalArena';
+            trial.xsg.xsgFilePath = 'fixtures/EC20111018/Imaging/AA0001AAAA0001.xsg';
+            
+            [epoch,importXSG] = insertFlyArenaEpoch(self.epochGroup,...
+                trial);
+            
+            assert(importXSG, 'Should import XSG file');
+            
+            stim = epoch.getStimulus('Fly Arena');
+            
+            xsgX = stim.getMyProperty('xsgXSequenceResponse');
+            assert(~isempty(xsgX));
+            
+            xsgY = stim.getMyProperty('xsgYSequenceResponse');
+            assert(~isempty(xsgY));
+        end
         
-        %TODO: resources
-        %TODO: xsg channel links
-        %TODO: responses
+        function testShouldSkipXSGChannelLinksIfMissingFromTrialStruct(self)
+            protocolID = 'testShouldSkipXSGChannelLinksIfMissingFromTrialStruct';
+            
+            trial = self.trialStruct(protocolID);
+            
+            [epoch,~] = insertFlyArenaEpoch(self.epochGroup,...
+                trial);
+            
+            stim = epoch.getStimulus('Fly Arena');
+            
+            xsgX = stim.getMyProperty('xsgXSequenceResponse');
+            assert(isempty(xsgX));
+            
+            xsgY = stim.getMyProperty('xsgYSequenceResponse');
+            assert(isempty(xsgY));
+        end
+        
+        function testShouldThrowErrorIfFraneNumberAndXSGSequenceAreMissing(self)
+            protocolID = 'testShouldThrowErrorIfFraneNumberAndXSGSequenceAreMissing';
+            
+            trial = self.trialStruct(protocolID);
+            trial.arena = rmfield(trial.arena, 'frameNumber');
+            
+            caught = false;
+            try
+                [~,~] = insertFlyArenaEpoch(self.epochGroup,...
+                    trial);
+            catch MException
+                assert(strcmp(MException.identifier, 'ovation:fly_arena_import:missingConfiguration'));
+                caught = true;
+            end
+            
+            assert(caught, 'Should throw error');
+        end
+        
+        function testShouldImportDirectResponseData(self)
+            import ovation.*;
+            
+            protocolID = 'testShouldImportDirectResponseData';
+            
+            trial = self.trialStruct(protocolID);
+            
+            trial.responses.channels(1).deviceName = 'dev1';
+            trial.responses.channels(1).deviceManufacturer = 'dev1Manufacturer';
+            trial.responses.channels(1).channelName = 'channelName1';
+            trial.responses.channels(1).samplingRateHz = 1000;
+            trial.responses.channels(1).data = rand(1,1000);
+            trial.responses.channels(1).units = 'A';
+            trial.responses.channels(1).deviceParameters.foo = 'bar';
+            
+            trial.responses.channels(2).deviceName = 'dev2';
+            trial.responses.channels(2).deviceManufacturer = 'dev2Manufacturer';
+            trial.responses.channels(2).channelName = 'channelName2';
+            trial.responses.channels(2).samplingRateHz = 1000;
+            trial.responses.channels(2).dataFileURL = 'file://TestArenaImportPattern.mat';
+            trial.responses.channels(2).shape = [1,1000];
+            trial.responses.channels(2).dataType = NumericDataType(NumericDataFormat.FloatingPointDataType,...
+                8,...
+                NumericByteOrder.ByteOrderLittleEndian);
+            trial.responses.channels(2).units = 'V';
+            trial.responses.channels(2).deviceParameters.foo = 'bar';
+            
+            
+            [epoch,~] = insertFlyArenaEpoch(self.epochGroup,...
+                trial);
+            
+            r1 = epoch.getResponse(trial.responses.channels(1).deviceName);
+            assert(~isempty(r1));
+            self.assertDirectResponse(r1, trial.responses.channels(1));
+            
+            r2 = epoch.getResponse(trial.responses.channels(2).deviceName);
+            assert(~isempty(r2));
+            self.assertDirectResponse(r2, trial.responses.channels(2));
+        end
+        
+        function testShouldThrowErrorIfDirectResponseDataAndURLMissing(self)
+            protocolID = 'testShouldImportDirectResponseData';
+            
+            trial = self.trialStruct(protocolID);
+            
+            trial.responses.channels(1).deviceName = 'dev1';
+            trial.responses.channels(1).deviceManufacturer = 'dev1Manufacturer';
+            trial.responses.channels(1).channelName = 'channelName1';
+            trial.responses.channels(1).samplingRateHz = 1000;
+            trial.responses.channels(1).units = 'A';
+            
+            
+            caught = false;
+            try
+                [~,~] = insertFlyArenaEpoch(self.epochGroup,...
+                    trial);
+            catch MException
+                assert(strcmp(MException.identifier, 'ovation:fly_arena_import:missingConfiguration'));
+                caught = true;
+            end
+            
+            assert(caught, 'Should throw exception');
+        end
+        
     end
+    
+    methods(Static)
+        function assertResource(resourceNames, filePath)
+            [~,expectedName,ext] = fileparts(filePath);
+            
+            names = cell(1,length(resourceNames));
+            for i = 1:length(resourceNames)
+                names{i} = char(resourceNames(i));
+            end
+            
+            assert(any(strcmp(names, [expectedName ext])),...
+                ['Expected: ' expectedName ext]);
+        end
         
+        function assertDirectResponse(r, trialResponseChannel)
+            import ovation.*;
+            
+            assert(strcmp(trialResponseChannel.deviceName, ...
+                char(r.getExternalDevice().getName())));
+            assert(strcmp(trialResponseChannel.deviceManufacturer, ...
+                char(r.getExternalDevice().getManufacturer())));
+            
+           
+            if(isfield(trialResponseChannel, 'data') &&...
+                    ~isempty(trialResponseChannel.data))
+                actual = r.getData();
+                expected = NumericData(trialResponseChannel.data);
+                
+                assert(isequal(actual.getShape(), expected.getShape()));
+                assert(isequal(actual.getFloatingPointData(), expected.getFloatingPointData()));
+            else
+                assert(strcmp(trialResponseChannel.dataFileURL, char(r.getURL())));
+                assert(isequal(trialResponseChannel.shape, r.getShape()'));
+            end
+            
+            assert(strcmp(trialResponseChannel.units, char(r.getUnits())));
+            srates = r.getSamplingRates();
+            assert(trialResponseChannel.samplingRateHz == srates(1));
+            srateUnits = r.getSamplingUnits();
+            assert(strcmp('Hz', char(srateUnits(1))));
+            
+            labels = r.getDimensionLabels();
+            assert(strcmp(trialResponseChannel.channelName, char(labels(1))));
+            
+            assert(r.getUTI() == Response.NUMERIC_DATA_UTI);
+            
+            params = ovation.map2struct(r.getDeviceParameters());
+            assert(strcmp(params.foo, trialResponseChannel.deviceParameters.foo));
+        end
+    end
+    
 end
