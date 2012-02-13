8ee164c51637b7a5594d7c82041b90aed4be5bcc
Barry Wark <barrywark@gmail.com>
2012-02-13 11:13:08 -0500

    Adding external sources to Jenkins harness

--- test/jenkins-test.sh
+++ test/jenkins-test.sh
@@ -4,5 +4,5 @@ cd test
 echo "Running tests with $MATLAB..."
 export PATH=$MATLAB_ROOT/bin:$PATH
 touch startup.m
-matlab -nodisplay -nodesktop -r "addpath(getenv('OVATION_MATLAB'));addpath(getenv('IMPORT_SRC_DIR'));addpath(getenv('MATLAB_XUNIT_PATH'));javaaddpath(getenv('OVATION_JAR_PATH'));ovation.OvationMatlabStartup(); runtestsuite test; exit(0)"
+matlab -nodisplay -nodesktop -r "addpath(getenv('OVATION_MATLAB'));addpath(getenv('IMPORT_SRC_DIR'));addpath(getenv('MATLAB_XUNIT_PATH'));addpath(getenv('EXTERNALS_SRC_DIR'));javaaddpath(getenv('OVATION_JAR_PATH'));ovation.OvationMatlabStartup(); runtestsuite test; exit(0)"
 cd ..
