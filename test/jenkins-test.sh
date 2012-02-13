366002e5fe324cb83a17bb22abcb3bb0af785a80
Barry Wark <barrywark@gmail.com>
2012-02-13 11:21:17 -0500

    Jenkins harness script
    Work from repo root directory.

--- test/jenkins-test.sh
+++ test/jenkins-test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd test
 echo "Running tests with $MATLAB..."
 export PATH=$MATLAB_ROOT/bin:$PATH
 touch startup.m
 matlab -nodisplay -nodesktop -r "addpath(getenv('OVATION_MATLAB'));addpath(getenv('IMPORT_SRC_DIR'));addpath(getenv('MATLAB_XUNIT_PATH'));addpath(getenv('EXTERNALS_SRC_DIR'));javaaddpath(getenv('OVATION_JAR_PATH'));ovation.OvationMatlabStartup(); runtestsuite test; exit(0)"
-cd ..
